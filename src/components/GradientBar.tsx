740dcdf63661617bb447045b117983ffa7629497
Kiguni001 <Theeradask9988@gmail.com>
2025-07-23 04:53:55 +0700

    finish01

--- src/components/GradientBar.tsx
+++ src/components/GradientBar.tsx
@@ -1,6 +1,3 @@
-// src/components/GradientBar.tsx
-import React from "react";
-
 const GradientBar = () => {
   return (
     <div className="h-[15vh] w-full bg-gradient-to-b from-[#040404] to-[#111111]" />
